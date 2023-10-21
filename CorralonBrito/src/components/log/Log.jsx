8fcafb0879ad951339c1baf4ef4158f5091effe7
GiuColella <colellag47@gmail.com>
2023-10-20 22:02:18 -0300

    implementacion de axios en el log

--- CorralonBrito/src/components/log/Log.jsx
+++ CorralonBrito/src/components/log/Log.jsx
@@ -1,31 +1,53 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import StyleLog from './log.module.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import StyleLog from './log.module.css';
+import axios from 'axios';
 
 export default function Log() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/your-api-url', { username, password });
+      // handle your response here
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className={StyleLog.box}>
-        <div className={StyleLog.contenedor}>
-          <div className={StyleLog.contenedor__superior}>
-            {/* Parte superior = LOGIN */}
-            <div className={''}>
-              {/* Logo */}
-              <div className={StyleLog.logo}></div>
-            </div>
-            <div className={''}>
-              {/* Nombre de la Sucursal */}
-              <h1>CorralonBrito</h1>
-            </div>
+      <div className={StyleLog.contenedor}>
+        <div className={StyleLog.contenedor__superior}>
+          <div className={''}>
+            <div className={StyleLog.logo}></div>
           </div>
-          <div className={StyleLog.contenedor__inferior}>
-            {/* Parte inferior = LOGIN */}
-            <form className={StyleLog.formulario} action=''>
-              <input className={StyleLog.formulario__imput} type='text' placeholder='Usuario' value={''} />
-              <input className={StyleLog.formulario__imput} type='text' placeholder='Contraseña' value={''} />
-              <Link to="/home" className={StyleLog.formulario__boton}>Ingresar</Link>
-            </form>
+          <div className={''}>
+            <h1>CorralonBrito</h1>
           </div>
         </div>
+        <div className={StyleLog.contenedor__inferior}>
+          <form className={StyleLog.formulario} onSubmit={handleSubmit}>
+            <input
+              className={StyleLog.formulario__imput}
+              type='text'
+              placeholder='Usuario'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              className={StyleLog.formulario__imput}
+              type='password'
+              placeholder='Contraseña'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className={StyleLog.formulario__boton}>Ingresar</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
